b4f28936b5853b7ff0dbc7db82294e8b3cb8dd5f
Noor Care <50613234+NoorCare@users.noreply.github.com>
2019-09-04 17:38:04 +0530

    Created

--- app/src/main/java/com/eviv3k/movies/MainActivity.kt
+++ app/src/main/java/com/eviv3k/movies/MainActivity.kt
@@ -2,11 +2,36 @@ package com.eviv3k.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar!!.title = "Movies"
+        actionBar.elevation = 4.0F
+        actionBar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inference = menuInflater
+        inference.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_notification -> {
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
